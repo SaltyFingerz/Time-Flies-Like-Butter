8ff5d5d696b1fdab2bbbef1311f5283c9906c8b6
SaltyFingerz <denis.iannoukos@yandex.com>
2023-10-24 14:22:12 +0300

    metamorphosis animation with event and flying controller

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -7,51 +7,133 @@ public class PlayerMovement : MonoBehaviour
     //www.youtube.com/watch?v=K1xZ-rycYY8&t=42s
 
     private float horizontal;
+    private float vertical;
     private float speed = 8f;
     private float jumpingPower = 16f;
     private bool isFacingRight = true;
+    private bool canMorph = false;
 
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Animator anim;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
+
+    public enum LifeStage {caterpillar = 0, butterfly = 1, dead = 2};
+    public LifeStage lifeStage = LifeStage.caterpillar;
     // Start is called before the first frame update
-  
+
 
     // Update is called once per frame
     void Update()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
-        if(Input.GetButtonDown("Jump") && isGrounded())
-        {
-            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
-        }
+        vertical = Input.GetAxisRaw("Vertical");
 
-        if(Input.GetButtonUp("Jump") && rb.velocity.y >0f)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
-        }
+        StateCheck();
 
-        Flip();
+      
 
-        if(rb.velocity.x != 0 && isGrounded())
+            
+           
+        
+
+        
+
+
+
+            Flip();
+
+       
+    }
+
+    void StateCheck()
+    {
+        switch (lifeStage)
         {
-            anim.SetBool("Walking", true);
-        }
-        else
-        {
-            anim.SetBool("Walking" ,false);
+            case LifeStage.caterpillar:
+
+                if (Input.GetButtonDown("Jump") && isGrounded())
+                {
+                    rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+                }
+
+                if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
+                {
+                    rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+                }
+
+                if (Input.GetKeyDown(KeyCode.Q))
+
+                {
+                    Debug.Log("MorphingTime");
+                    canMorph = true;
+                    Metamorphosis();
+
+                }
+
+                if (rb.velocity.x != 0 && isGrounded())
+                {
+                    anim.SetBool("Walking", true);
+                }
+                else
+                {
+                    anim.SetBool("Walking", false);
+                }
+
+                break;
+
+            case LifeStage.butterfly:
+
+                rb.gravityScale = 0f;
+
+                /*if (rb.velocity.x != 0 || rb.velocity.y != 0)
+                {
+                    //anim flying.
+                }
+                else
+                {
+                    anim.SetBool("Walking", false);
+                }*/
+
+                break;
+
+            case LifeStage.dead:
+             
+                break;
+
+            default:
+                break;
         }
     }
 
     private void FixedUpdate()
     {
-       rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+        if (lifeStage == LifeStage.caterpillar)
+        {
+            rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+        }
+        if(lifeStage== LifeStage.butterfly)
+        {
+            rb.velocity = new Vector2(horizontal * speed, vertical * speed);
+        }
     }
 
     private bool isGrounded()
     {
         return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+    public void Metamorphosis()
+    {
+        if (canMorph)
+        {
+            anim.SetBool("Morph", true);
+        }
+    }
+
+    public void MorphEvent()
+    {
+        lifeStage = LifeStage.butterfly;
     }
 
     private void Flip()
