b13edff4abf562dd74424ddaa66485470779f0da
SaltyFingerz <denis.iannoukos@yandex.com>
2023-11-30 15:10:21 +0000

    made pollen more vivid, adjusted collider on demo scene

--- Assets/SadElfScript.cs
+++ Assets/SadElfScript.cs
@@ -28,9 +28,9 @@ public class SadElfScript : MonoBehaviour
     IEnumerator StopTearsCoroutine()
     {
         tears.Stop();
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(1f);
         splash.SetActive(false);
-        yield return new WaitForSeconds(3f);
+        yield return new WaitForSeconds(2f);
         SceneManager.LoadScene("TeleportLevel");
 
     }
