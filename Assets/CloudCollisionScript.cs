4d190565eebfd83714658ee2e1462bf558cc9310
SaltyFingerz <denis.iannoukos@yandex.com>
2024-03-27 15:45:47 +0000

    updated level selection map with branches and new levels, still needs complete functionality update, changed order for narrative cohesion, and opening panel of fan level and butterfly wind

--- Assets/CloudCollisionScript.cs
+++ Assets/CloudCollisionScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CloudCollisionScript : MonoBehaviour
 {
@@ -16,7 +17,7 @@ public class CloudCollisionScript : MonoBehaviour
             // Spawn an explosion at each point of contact
             Vector2 hitPoint = coll.contacts[0].point;
             Instantiate(explosion, new Vector3(hitPoint.x, hitPoint.y, 0), Quaternion.identity);
-
+            StartCoroutine(LevelEnd());
            /* foreach (ContactPoint2D missileHit in coll.contacts)
                 {
              
@@ -28,5 +29,14 @@ public class CloudCollisionScript : MonoBehaviour
            */
         }
       
+    }
+
+    IEnumerator LevelEnd()
+    {
+        PlayerPrefs.SetInt("Level", SceneManager.GetActiveScene().buildIndex);
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene("LevelSelectMap");
+      
+
     }
 }
