1460338817995169fd866e981cb40901ea3852b5
SaltyFingerz <denis.iannoukos@yandex.com>
2024-04-03 14:29:52 +0300

    pudding polaroids implemented

--- Assets/PuddingManager.cs
+++ Assets/PuddingManager.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 public class PuddingManager : MonoBehaviour
 {
     [SerializeField] private GameObject FacesObject;
+    [SerializeField] private GameObject Polaroids;
 
     public void StartConversation()
     {
         FacesObject.GetComponent<Animator>().SetTrigger("Talk");
+    }
+
+    public void PlayPolaroids()
+    {
+        Polaroids.SetActive(true);
     }
 }
